676b1f2979ce3efcd8d05b2b50c2b01ceb43b8ba
umesh2407 <choudharyumesh986@gmail.com>
2024-08-30 21:03:05 +0530

    working

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
   <>
   <div className='fixed  w-full h-screen'>
-  <div className='absolute top-[5%] w-full py-8 flex justify-center text-zinc-600 text-xl font-semibold'>QuikNotz.</div>
+  <div className='absolute top-[5%] w-full py-8 flex justify-center text-zinc-600 text-xl font-semibold'>QuikNotz. @Copyrights Umesh Choudhary</div>
   <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>QuikNotz.
   </h1>
   </div>
